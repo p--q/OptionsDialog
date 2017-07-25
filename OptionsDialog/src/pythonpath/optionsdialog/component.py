8dd51507a56ecf86d7c1dac45c1a0171fb828ed4
pq <pq.blogspot@gmail.com>
2017-07-25 13:33:37 +0900

    _()の導入。

--- OptionsDialog/src/pythonpath/optionsdialog/component.py
+++ OptionsDialog/src/pythonpath/optionsdialog/component.py
@@ -32,15 +32,15 @@ class DilaogHandler(unohelper.Base, XServiceInfo, XContainerWindowEventHandler):
 					maxwidth, maxheight = self.readConfig(*self.cfgnames)  # コンポーネントデータノードの値を取得。取得した値は文字列。
 					buttonlistener = ButtonListener(dialog)  # ボタンリスナーをインスタンス化。
 					addControl = controlCreator(self.ctx, self.smgr, dialog)  # オプションダイアログdialogにコントロールを追加する関数を取得。
-					addControl("FixedLine", {"PositionX": 5, "PositionY": 13, "Width": 250, "Height": 10, "Label": "Maximum page size"})  # 文字付き水平線。
-					addControl("FixedText", {"PositionX": 11, "PositionY": 39, "Width": 49, "Height": 15, "Label": "Width", "NoLabel": True})  # 文字列。
+					addControl("FixedLine", {"PositionX": 5, "PositionY": 13, "Width": 250, "Height": 10, "Label": _("Maximum page size")})  # 文字付き水平線。
+					addControl("FixedText", {"PositionX": 11, "PositionY": 39, "Width": 49, "Height": 15, "Label": _("Width"), "NoLabel": True})  # 文字列。
 					addControl("NumericField", {"PositionX": 65, "PositionY": 39, "Width": 60, "Height": 15, "Spin": True, "ValueMin": 1, "Value": float(maxwidth), "DecimalAccuracy": 2, "HelpText": "Width"})  # 上下ボタン付き数字枠。小数点2桁、floatに変換して値を代入。
 					addControl("NumericField", {"PositionX": 65, "PositionY": 64, "Width": 60, "Height": 15, "Spin": True, "ValueMin": 1, "Value": float(maxheight), "DecimalAccuracy": 2, "HelpText": "Height"})  # 同上。
-					addControl("FixedText", {"PositionX": 11, "PositionY": 66, "Width": 49, "Height": 15, "Label": "Height", "NoLabel": True})  # 文字列。
+					addControl("FixedText", {"PositionX": 11, "PositionY": 66, "Width": 49, "Height": 15, "Label": _("Height"), "NoLabel": True})  # 文字列。
 					addControl("FixedText", {"PositionX": 127, "PositionY": 42, "Width": 25, "Height": 15, "Label": "cm", "NoLabel": True})  # 文字列。
 					addControl("FixedText", {"PositionX": 127, "PositionY": 68, "Width": 25, "Height": 15, "Label": "cm", "NoLabel": True})  # 文字列。
-					addControl("Button", {"PositionX": 155, "PositionY": 39, "Width": 50, "Height": 15, "Label": "~Default"}, {"setActionCommand": "width", "addActionListener": buttonlistener})  # ボタン。
-					addControl("Button", {"PositionX": 155, "PositionY": 64, "Width": 50, "Height": 15, "Label": "~Default"}, {"setActionCommand": "height", "addActionListener": buttonlistener})  # ボタン。
+					addControl("Button", {"PositionX": 155, "PositionY": 39, "Width": 50, "Height": 15, "Label": _("~Default")}, {"setActionCommand": "width", "addActionListener": buttonlistener})  # ボタン。
+					addControl("Button", {"PositionX": 155, "PositionY": 64, "Width": 50, "Height": 15, "Label": _("~Default")}, {"setActionCommand": "height", "addActionListener": buttonlistener})  # ボタン。
 				elif eventname=="ok":  # OKボタンが押された時
 					maxwidth = dialog.getControl("NumericField1").getModel().Value  # NumericFieldコントロールから値を取得。
 					maxheight = dialog.getControl("NumericField2").getModel().Value  # NumericFieldコントロールから値を取得。
