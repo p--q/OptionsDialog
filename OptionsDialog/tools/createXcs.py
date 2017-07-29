56bf37a84154bc2e7d02d040b7c6ef1fadffd279
pq <pq.blogspot@gmail.com>
2017-07-29 20:32:55 +0900

    config.xcsにDefaultsノードの追加。

--- OptionsDialog/tools/createXcs.py
+++ OptionsDialog/tools/createXcs.py
@@ -11,16 +11,19 @@ def createXcs(c):
 	with open(filename, "w", encoding="utf-8") as f:  # OptionsDialog.xcuファイルの作成
 		root = Elem("oor:component-schema", {"oor:name": "ExtensionData", "oor:package": c["ExtentionID"], "xmlns:oor": "http://openoffice.org/2001/registry", "xmlns:xs": "http://www.w3.org/2001/XMLSchema", "xmlns:xsi": "http://www.w3.org/2001/XMLSchema-instance", "xml:lang": "en-US"})  # 根の要素を作成。
 		root.append(Elem("templates"))
-		root[-1].append(Elem("group", {"oor:name": "Size"}))
+		root[-1].append(Elem("group", {"oor:name": "Sizes"}))
 		root[-1][-1].append(Elem("info"))
 		root[-1][-1][-1].append(Elem("desc", text="width and hight."))
 		root[-1][-1].append(Elem("prop", {"oor:name": "Width", "oor:type": "xs:string"}))
-		root[-1][-1][-1].append(Elem("value", text="300"))
 		root[-1][-1].append(Elem("prop", {"oor:name": "Height", "oor:type": "xs:string"}))		
-		root[-1][-1][-1].append(Elem("value", text="300"))
+		root[-1][-1].append(Elem("group", {"oor:name": "Defaults"}))		
+		root[-1][-1][-1].append(Elem("prop", {"oor:name": "Width", "oor:type": "xs:string"}))
+		root[-1][-1][-1][-1].append(Elem("value", text="300"))
+		root[-1][-1][-1].append(Elem("prop", {"oor:name": "Height", "oor:type": "xs:string"}))
+		root[-1][-1][-1][-1].append(Elem("value", text="300"))		
 		root.append(Elem("component"))
 		root[-1].append(Elem("group", {"oor:name": "Leaves"}))
-		root[-1][-1].append(Elem("node-ref", {"oor:name": "MaximumPaperSize", "oor:node-type": "Size"}))
+		root[-1][-1].append(Elem("node-ref", {"oor:name": "MaximumPaperSize", "oor:node-type": "Sizes"}))
 		tree = ET.ElementTree(root)  # 根要素からxml.etree.ElementTree.ElementTreeオブジェクトにする。
 		tree.write(f.name, "utf-8", True)  # xml_declarationを有効にしてutf-8でファイルに出力する。   
 	print("{} has been created.".format(filename))	
